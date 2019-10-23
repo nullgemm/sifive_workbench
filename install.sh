e1a73dbc092d1e14fa53fe8457ae52b7e8b4ec0d
nullgemm <nullgemm@mailbox.org>
2019-10-23 11:12:36 +0200

    auto-updating install script

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-gcc=riscv64-unknown-elf-gcc-8.2.0-2019.02.0-x86_64-linux-ubuntu14
-ocd=riscv-openocd-0.10.0-2019.02.0-x86_64-linux-ubuntu14
+wget https://www.sifive.com/boards
 
-curl -O https://static.dev.sifive.com/dev-tools/$gcc.tar.gz
-curl -O https://static.dev.sifive.com/dev-tools/$ocd.tar.gz
+gcc=$(grep -oP \
+"riscv64-unknown-elf-gcc-"\
+"[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\.[0-9]+\.[0-9]+"\
+"-x86_64-linux-ubuntu[0-9]+" boards | tail -n 1)
+
+ocd=$(grep -oP \
+"riscv-openocd-"\
+"[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\.[0-9]+\.[0-9]+"\
+"-x86_64-linux-ubuntu[0-9]+" boards | tail -n 1)
+
+wget https://static.dev.sifive.com/dev-tools/$gcc.tar.gz
+wget https://static.dev.sifive.com/dev-tools/$ocd.tar.gz
+
 tar -xvzf $gcc.tar.gz
 tar -xvzf $ocd.tar.gz
 mv $gcc riscv64-unknown-elf-gcc
 mv $ocd riscv-openocd
 rm $gcc.tar.gz
 rm $ocd.tar.gz
+rm boards
 
 git clone --recurse-submodules https://github.com/sifive/freedom-e-sdk.git
