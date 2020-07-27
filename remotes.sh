9937a6820a9028d5ef267d0a8396c6fc9291bd28
nullgemm <nullgemm@mailbox.org>
2020-07-27 20:40:14 +0200

    update

--- remotes.sh
+++ remotes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git remote add github git@github.com:cylgom/sifive_workbench.git
-git remote add gitea ssh://git@git.cylgom.net:2999/cylgom/sifive_workbench.git
+git remote add github git@github.com:nullgemm/sifive_workbench.git
+git remote add gitea ssh://git@git.nullgemm.fr:2999/nullgemm/sifive_workbench.git
